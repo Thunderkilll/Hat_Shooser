cd289063437a09c80a969b1e790ce11947eaab4d
unknown <thunderkilll>
2019-08-30 15:40:49 +0200

    t

---
+++ Assets/CoherentUI/Code/Scripts/HatSelection.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace KhaledGuesmi
+{
+    public class HatSelection : MonoBehaviour
+    {
+        public GameObject hat1;
+        public GameObject hat2;
+        public GameObject hat3;
+        public GameObject hat4;
+
+
+        public void Hat1Selected()
+        {
+            hat1.SetActive(true);
+            hat2.SetActive(false);
+            hat3.SetActive(false);
+            hat4.SetActive(false);
+        }
+        public void Hat2Selected()
+        {
+            hat1.SetActive(false);
+            hat2.SetActive(true);
+            hat3.SetActive(false);
+            hat4.SetActive(false);
+        }
+        public void Hat3Selected()
+        {
+            hat1.SetActive(false);
+            hat2.SetActive(false);
+            hat3.SetActive(true);
+            hat4.SetActive(false);
+        }
+        public void Hat4Selected()
+        {
+            hat1.SetActive(false);
+            hat2.SetActive(false);
+            hat3.SetActive(false);
+            hat4.SetActive(true);
+        }
+    }
+}
+
